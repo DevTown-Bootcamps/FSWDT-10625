3084bec4d10e810105aaa21d5052c04a68b49c5c
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-06-25 21:20:30 +0530

    day-10

---
+++ javascript/day-10.js
@@ -0,0 +1,120 @@
+// variables
+
+// Datatype
+// Intgers: -Infinity to +Infinity
+// long:long form of Integers
+// double or float:decimal
+// Boolean:true/false;
+// String:word/sentence
+
+// How we create variables
+
+// let blocked scoped variables
+// let name=true;
+// let a =10;
+// let fname="Anshul";
+// let b=10.55;
+
+
+// {
+//     let sum=0;
+// }
+
+// console.log(sum);
+
+// Const variable
+// it cannot get updated later in the code-blocked variable
+// const a =10;
+// console.log(a);
+
+// var variable
+// var a=20;
+// console.log(a);
+// a=30;
+// console.log(a);
+
+// {
+//     var b=20;
+// }
+
+// console.log(b);
+
+
+// Datatype
+// Primtive data type
+
+// String
+// let name="Anshul@1234-/**/*/*/*";
+// console.log(typeof name);
+
+// Numbers
+// let age=30;
+// console.log(typeof age);
+
+// Boolean
+// let a=true;
+// let b=false;
+
+// console.log(typeof a);
+// console.log(typeof b);/
+
+// undefined
+// let a;
+// console.log(a);
+
+// null
+// let b=null;
+// console.log(b);
+
+// Bigint
+
+// let bignum=12344678912345678912345678912344567809876765543321111345678865433;
+// console.log(typeof bignum);
+
+
+// Non-primivite
+// Object
+
+// let person={
+//     name:'Anshul',
+//     age:22,
+//     marks:87
+// }
+
+// console.log(person.marks);
+
+// Arrays
+let fruit=["cherry",'apple',10,20,true];
+
+console.log(fruit);
+
+// Operators
+
+// Arithmatic operators-math operation
+let a=30;
+let b=20;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+// Remender
+console.log(a%b);
+
+
+// Assignment operators
+
+let c=20;
+
+// Short hand assignment
+c=c+2;
+c +=2;
+
+c=c-2;
+c-=2;
+
+x=x*2;
+x*=2;
+
+x=x/2;
+x/=2;
