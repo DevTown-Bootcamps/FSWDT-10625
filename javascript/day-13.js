b59f507a27913c739fe4e56f86b507e62a1c78fa
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-07-01 20:24:49 +0530

    day-13

---
+++ javascript/day-13.js
@@ -0,0 +1,54 @@
+// Loop in js
+
+// For loop
+
+// for(intialization,condition,updation)
+// a++; ++a; incremental statement a=a=+1;
+//i=0; true; execution of statement ; i=1
+//i=1; true; execute; i=2;
+//i=2; true; execute; i=3;
+//i=3; true; execute; i=4;
+//i=4; true; execute; i=5;
+//i=5;true;execute; i=6;
+// i=6; false; end the loop
+
+
+// for(let i=0;i<=5;i++){
+//     console.log(i);
+// }
+
+// let fruit=["apple","bannana","cherry"];
+
+// for(let i=0;i<fruit.length;i++){
+//      console.log(fruit[i]);
+// }
+
+// let nums=[1,2,3,4,5,6];
+
+// for(let j=0;j<nums.length;j++){
+//     nums[j]=nums[j]+2; 
+//     //nums[j]+=2;
+//     console.log(nums[j]);
+// }
+
+//break;
+// range=1-100
+// print all numbers till you reach first prime number;
+
+// for(let i=0;i<=5;i++){
+//    console.log(i);
+//    if(i==3){
+//     break;
+//    }
+// }
+
+// for(let i=0;i<=5;i++){
+//    if(i==4){
+//     continue;
+//    }
+//    console.log(i);
+// }
+
+//create a array of number from 1 to 10, 
+// and update each element by nums[i]=nums[i]*2; untill the index of array is 7;
+
