df44e68e64b39a1e0c3a21ca969e6d3b1756d632
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-07-07 20:21:31 +0530

    day-17

---
+++ javascript/day-17.js
@@ -0,0 +1,57 @@
+//Functions-reusable code block to perform a certain task.
+
+//function function_name(paramter){
+    //code block for the function
+//}
+
+// function greet(){
+//     console.log("Hello");
+// }
+
+
+// greet();
+// greet();
+// greet();
+
+
+//passing paramter value to function
+
+// function add(a,b){
+//     return a+b;
+// }
+
+
+
+//console.log(add(2,3));
+
+// function mul(){
+//     return add(3,4)*2;
+// }
+
+// console.log(mul());
+
+
+//function expression
+
+// let add= function(a,b){
+//     return a+b;
+// }
+
+// console.log(add(3,2));
+
+
+//arrow function
+// const greet = () =>{
+//     console.log("hello");
+// }
+
+// greet();
+
+// const square = (a) =>{
+//   return a*a;
+// }
+
+// console.log(square(2));
+
+//default paramter
+
