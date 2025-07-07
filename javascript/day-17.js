66ad1568e6dbad9b9b4bcfbe88bdff8640dd6c66
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-07-07 20:55:40 +0530

    day-17

--- javascript/day-17.js
+++ javascript/day-17.js
@@ -51,7 +51,66 @@
 //   return a*a;
 // }
 
+// const sqaure(n){
+//     return n*n;
+// }
+
 // console.log(square(2));
 
 //default paramter
+// function greet(name = "Guest"){
+//     console.log(name);
+// }
+
+// greet();
+// greet('Anshul');
+
+
+// function mul(a=3,b=4){
+//     console.log(a*b);
+// }
+
+// mul();
+// mul(2);
+// mul(4,5);
+
+
+//Take multiple parameter- I do not know number of paramters
+
+// function sum(...n){
+//     let sum=0;
+
+//     for(let num of n){
+//       sum+=num;
+//     }
+//     console.log(sum);
+// }
+
+// sum(1,2,3);
+// sum(1,2,3,4,5);
+// sum(1,2,3,4,5,6);
+
+//Create a function to compute the cube of the number, using arrow function.Pass n as a paramter.
+// Use default paramter in this case.
+
+//Take n number of paraters and compute multiplication of these numbers.
+
+//scope of function
+
+// function mess(){
+//   let message="I am there";
+//   console.log(message);
+// }
+// mess();
+
+
+const prd = (...number) => {
+    let result=1;
+
+    for(let i=0;i<number.length;i++){
+        result*=number[i];
+    }
+    console.log(result);
+}
+prd(2,3,4,5);
 
