90a79b69ae04ad989ef5cc709087df6a8c3e1e02
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-07-24 16:01:19 +0530

    day-24

---
+++ nodejs/Math.js
@@ -0,0 +1,18 @@
+function add(a,b){
+    return a+b;
+}
+
+function mul(a,b){
+    return a*b;
+}
+
+function check(a){
+    if(a%2===0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+module.exports={add,mul,check};
+
---
+++ nodejs/day-24.js
@@ -0,0 +1,10 @@
+//local module
+
+const math=require("./Math");
+
+console.log(math.add(2,3));
+console.log(math.mul(2,3));
+
+console.log(math.check(5));
+
+// create modules for division and subtraction of two numbers
