cb8b1e1a19488e583bac950ee6ee83d457c0d6a2
[200~anshulojha1214@gmail.com <anshulojha1214@gmail.com>
2025-07-30 20:54:29 +0530

    express server

--- nodejs/day-26.js
+++ nodejs/day-26.js
@@ -1,18 +1,35 @@
 const http=require('http');
-// const express=require('express');
-// const app=express();
+const express=require('express');
+const app=express();
 
-http.createServer((req,res)=>{
-    if(req.url==="/"){
-        res.end("Home page");
-    }else if(req.url==="/about"){
-        res.end("About page");
-    }else{
-        res.end("page not found");
-    }
-}).listen(3000,()=>{
-    console.log("Server running on port 3000");
+// http.createServer((req,res)=>{
+//     if(req.url==="/"){
+//         res.end("Home page");
+//     }else if(req.url==="/about"){
+//         res.end("About page");
+//     }else if(req.url==="/contact"){
+//         res.end("Contact page");
+//     }else{
+//         res.end("page not found");
+//     }
+// }).listen(8080,()=>{
+//     console.log("Server running on port 8080");
+// })
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the home page");
 })
+
+
+app.get("/about",(req,res)=>{
+    res.send("Wlecome to about page");
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("Welcome to contact page");
+})
+
+app.listen(8080,()=>{console.log("Server running on port 8080")});
 
 
 
